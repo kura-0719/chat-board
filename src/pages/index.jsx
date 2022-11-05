a6511b9c8a88f491267a3976da59340676d6399f
takumi <takura010719@gmail.com>
2022-11-05 17:57:50 +0900

    🐛 バグの修正

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,5 @@
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
-import { Board } from "src/components/Board";
-import SendText from "src/components/SendText";
-import Tread from "src/components/SendText";
-import { useState } from "react";
 import Link from "next/link";
 
 const Threads = [
@@ -37,8 +33,8 @@ export default function Home() {
                   href={Threads.href}
                   className={styles.card_content}
                 >
-                  <p className={styles.card_title}>{Threads.title}</p>
-                  <p className={styles.card_txt}>{Threads.description}</p>
+                  <p key={Threads.title} className={styles.card_title}>{Threads.title}</p>
+                  <p key={Threads.description} className={styles.card_txt}>{Threads.description}</p>
                 </Link>
               </div>
             );
